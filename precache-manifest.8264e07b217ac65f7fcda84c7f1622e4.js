6e1ddd6b71c2ebf0ac4c3f987bbc511407c3d47d
Yuhan <hyuhan629@gmail.com>
2020-08-05 03:08:29 +0800

    Updates

---
+++ precache-manifest.8264e07b217ac65f7fcda84c7f1622e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cc6615e60e6a337e21795715a5c2fd2",
+    "url": "/hugdog-app/index.html"
+  },
+  {
+    "revision": "3c589d99145c5e36f800",
+    "url": "/hugdog-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "09ea19917b92b424ef75",
+    "url": "/hugdog-app/static/js/2.474eeb67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hugdog-app/static/js/2.474eeb67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c589d99145c5e36f800",
+    "url": "/hugdog-app/static/js/main.aca2c1ed.chunk.js"
+  },
+  {
+    "revision": "49a3d49a660d6edee488",
+    "url": "/hugdog-app/static/js/runtime-main.9b69a4f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hugdog-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
